8d8b1f0b63c80244523898818205f22a62a83863
Hunter Boleman <hunter.boleman@gmail.com>
2019-03-25 13:52:43 -0700

    Parsechat 1: Milestone 3: DONE

--- Parsechat/ChatViewController.swift
+++ Parsechat/ChatViewController.swift
@@ -7,24 +7,33 @@
 //
 
 import UIKit
+import Parse
 
 class ChatViewController: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet weak var chatMessageField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func doSendMessage(_ sender: Any) {
+        let chatMessage = PFObject(className: "Message");
+        chatMessage["text"] = chatMessageField.text ?? ""
+        
+        chatMessage.saveInBackground { (success, error) in
+            if success {
+                print("The message was saved!")
+                self.chatMessageField.text = "";
+            } else if let error = error {
+                print("Problem saving message: \(error.localizedDescription)")
+            }
+        }
     }
-    */
+    
+    
 
 }
