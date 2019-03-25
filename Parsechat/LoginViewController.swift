56d678c2c699c1909b561b929495b14b8b93368e
Hunter Boleman <hunter.boleman@gmail.com>
2019-03-25 13:26:21 -0700

    Parsechat 1: Milestone 2: DONE

--- Parsechat/LoginViewController.swift
+++ Parsechat/LoginViewController.swift
@@ -23,11 +23,118 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func doSignUp(_ sender: Any) {
+        if (isNotEmpty() == true){
         registerUser();
+        }
     }
     
     @IBAction func doLogin(_ sender: Any) {
-        loginUser();
+        if (isNotEmpty() == true){
+            loginUser();
+        }
+    }
+    
+    func isNotEmpty() -> Bool{
+        /*var alertController = UIAlertController(title: "Empty Fields!", message: "Please enter both a username and password", preferredStyle: .alert)
+        
+        // create a cancel action
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            // handle cancel response here. Doing nothing will dismiss the view.
+        }
+        // add the cancel action to the alertController
+        alertController.addAction(cancelAction)
+        
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            // handle response here.
+        }
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+ */
+        
+        if (usernameField.text?.isEmpty == true && passwordField.text?.isEmpty == true){
+            let alertController = UIAlertController(title: "Empty Fields!", message: "Please enter both a username and password", preferredStyle: .alert)
+            // create a cancel action
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+                // handle cancel response here. Doing nothing will dismiss the view.
+            }
+            // add the cancel action to the alertController
+            alertController.addAction(cancelAction)
+            
+            // create an OK action
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                // handle response here.
+            }
+            // add the OK action to the alert controller
+            alertController.addAction(OKAction)
+            present(alertController, animated: true) {
+                // optional code for what happens after the alert controller has finished presenting
+            }
+            return false;
+        }
+        else if (usernameField.text?.isEmpty == true) {
+            let alertController = UIAlertController(title: "Enter Username!", message: "Please enter a username!", preferredStyle: .alert)
+            // create a cancel action
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+                // handle cancel response here. Doing nothing will dismiss the view.
+            }
+            // add the cancel action to the alertController
+            alertController.addAction(cancelAction)
+            
+            // create an OK action
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                // handle response here.
+            }
+            // add the OK action to the alert controller
+            alertController.addAction(OKAction)
+            present(alertController, animated: true) {
+                // optional code for what happens after the alert controller has finished presenting
+            }
+            return false;
+        }
+        else if (passwordField.text?.isEmpty == true) {
+            let alertController = UIAlertController(title: "Enter Password!", message: "Please enter a password!", preferredStyle: .alert)
+            // create a cancel action
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+                // handle cancel response here. Doing nothing will dismiss the view.
+            }
+            // add the cancel action to the alertController
+            alertController.addAction(cancelAction)
+            
+            // create an OK action
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                // handle response here.
+            }
+            // add the OK action to the alert controller
+            alertController.addAction(OKAction)
+            present(alertController, animated: true) {
+                // optional code for what happens after the alert controller has finished presenting
+            }
+            return false;
+        }
+        return true;
+    }
+    
+    func alertDisplay(strTitle: String, strMessage: String){
+        let alertController = UIAlertController(title: strTitle, message: strMessage, preferredStyle: .alert)
+        
+        // create a cancel action
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            // handle cancel response here. Doing nothing will dismiss the view.
+        }
+        // add the cancel action to the alertController
+        alertController.addAction(cancelAction)
+        
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            // handle response here.
+        }
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        
+        present(alertController, animated: true) {
+            // optional code for what happens after the alert controller has finished presenting
+        }
     }
     
     
@@ -44,6 +151,7 @@ class LoginViewController: UIViewController {
         newUser.signUpInBackground { (success: Bool, error: Error?) in
             if let error = error {
                 print(error.localizedDescription)
+                self.alertDisplay(strTitle: "Alert!", strMessage: "Registration did not work, please try again later.")
             } else {
                 print("User Registered successfully")
                 // manually segue to logged in view
@@ -59,6 +167,7 @@ class LoginViewController: UIViewController {
         PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
             if let error = error {
                 print("User log in failed: \(error.localizedDescription)")
+                self.alertDisplay(strTitle: "Alert!", strMessage: "Invalid login, or server is down.")
             } else {
                 print("User logged in successfully")
                 // display view controller that needs to shown after successful login
