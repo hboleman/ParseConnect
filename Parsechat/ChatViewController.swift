18e35bab0903f9f38fd03759dc3928cd0dcf0aeb
Hunter Boleman <hunter.boleman@gmail.com>
2019-03-28 16:58:06 -0700

    Parse Chat 1: Cleaned up code

--- Parsechat/ChatViewController.swift
+++ Parsechat/ChatViewController.swift
@@ -15,62 +15,27 @@ class ChatViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var chatMessageField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
-    // Global Variables
+    // Master Message Object
     var chatMessages: [PFObject] = [];
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Needed for the UITableView
         tableView.dataSource = self as UITableViewDataSource
         // Auto size row height based on cell autolayout constraints
         tableView.rowHeight = UITableView.automaticDimension
         // Provide an estimated row height. Used for calculating scroll indicator
         tableView.estimatedRowHeight = 50
-        
-        Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.onTimer), userInfo: nil, repeats: true)
-        
+        // Sets getChatMessage to retrieve messages every 5 seconds
+        Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.getChatMessages), userInfo: nil, repeats: true)
+        // runs getChatMessages for the first time
         getChatMessages();
         print ("reload tableView")
         self.tableView.reloadData();
     }
     
-    @IBAction func doSendMessage(_ sender: Any) {
-        let chatMessage = PFObject(className: "Messages");
-        chatMessage["text"] = chatMessageField.text!
-        chatMessage["user"] = PFUser.current();
-        
-        chatMessage.saveInBackground { (success, error) in
-            if success {
-                print("The message was saved!")
-                self.chatMessageField.text = "";
-            } else if let error = error {
-                print("Problem saving message: \(error.localizedDescription)")
-            }
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return chatMessages.count;
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Reusable Cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell") as! ChatCell;
-        // gets a single message
-        let chatMessage = chatMessages[indexPath.row];
-        // Set text
-        cell.messageLable.text = chatMessage["text"] as? String;
-        //Set username
-        if let user = chatMessage["user"] as? PFUser {
-            // User found! update username label with username
-            cell.usernameLabel.text = user.username;
-        } else {
-            // No user found, set default username
-            cell.usernameLabel.text = "🤖"
-        }
-        return cell;
-    }
-    
-    func getChatMessages(){
+    // Gets Chat Messages
+    @objc func getChatMessages(){
         let query = PFQuery(className:"Messages")
         query.addDescendingOrder("createdAt")
         query.limit = 20
@@ -90,9 +55,44 @@ class ChatViewController: UIViewController, UITableViewDataSource {
         self.tableView.reloadData();
     }
     
-    @objc func onTimer() {
-        print("about to get chat messages")
-        getChatMessages();
+    // Sends The User's Message
+    @IBAction func doSendMessage(_ sender: Any) {
+        let chatMessage = PFObject(className: "Messages");
+        chatMessage["text"] = chatMessageField.text!
+        chatMessage["user"] = PFUser.current();
+        chatMessage.saveInBackground { (success, error) in
+            if success {
+                print("The message was saved!")
+                self.chatMessageField.text = "";
+            } else if let error = error {
+                print("Problem saving message: \(error.localizedDescription)")
+            }
+        }
     }
-
+    
+    // TABLE VIEW FUNCTIONS
+    
+    // Sets Table Rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatMessages.count;
+    }
+    
+    // Sets Table Cell Contents
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Reusable Cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell") as! ChatCell;
+        // gets a single message
+        let chatMessage = chatMessages[indexPath.row];
+        // Set text
+        cell.messageLable.text = chatMessage["text"] as? String;
+        //Set username
+        if let user = chatMessage["user"] as? PFUser {
+            // User found! update username label with username
+            cell.usernameLabel.text = user.username;
+        } else {
+            // No user found, set default username
+            cell.usernameLabel.text = "🤖"
+        }
+        return cell;
+    }
 }
--- Parsechat/LoginViewController.swift
+++ Parsechat/LoginViewController.swift
@@ -15,25 +15,70 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+    // Button Event for Sign-Up
     @IBAction func doSignUp(_ sender: Any) {
+        // Checks for valid text entry
         if (isNotEmpty() == true){
         registerUser();
         }
     }
-    
+
+    // Button Event for Login
     @IBAction func doLogin(_ sender: Any) {
+        // Checks for valid text entry
         if (isNotEmpty() == true){
             loginUser();
         }
     }
     
+    // Registers User
+    func registerUser() {
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameField.text
+        //newUser.email = emailField.text
+        newUser.password = passwordField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+                self.alertDisplay(strTitle: "Alert!", strMessage: "Registration did not work, please try again later.")
+            } else {
+                print("User Registered successfully")
+                // manually segue to logged in view
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    // Logs in User
+    func loginUser() {
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+                self.alertDisplay(strTitle: "Alert!", strMessage: "Invalid login, or server is down.")
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    // EXTRA FUNCTIONS
+    
+    // Logic for Testing if a Text Field is Empty or Not
     func isNotEmpty() -> Bool{
         if (usernameField.text?.isEmpty == true && passwordField.text?.isEmpty == true){
             let alertController = UIAlertController(title: "Empty Fields!", message: "Please enter both a username and password", preferredStyle: .alert)
@@ -98,65 +143,25 @@ class LoginViewController: UIViewController {
         return true;
     }
     
+    // Function Used By "isNotEmpty" to Easilty Call Alerts
     func alertDisplay(strTitle: String, strMessage: String){
+        // Creates Alert Object
         let alertController = UIAlertController(title: strTitle, message: strMessage, preferredStyle: .alert)
-        
-        // create a cancel action
+        // Creates a cancel action
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
             // handle cancel response here. Doing nothing will dismiss the view.
         }
-        // add the cancel action to the alertController
+        // Adds the cancel action to the alertController
         alertController.addAction(cancelAction)
-        
-        // create an OK action
+        // Creates an OK action
         let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
             // handle response here.
         }
-        // add the OK action to the alert controller
+        // Adds the OK action to the alert controller
         alertController.addAction(OKAction)
-        
+        // Shows the alert
         present(alertController, animated: true) {
             // optional code for what happens after the alert controller has finished presenting
-        }
-    }
-    
-    
-    func registerUser() {
-        // initialize a user object
-        let newUser = PFUser()
-        
-        // set user properties
-        newUser.username = usernameField.text
-        //newUser.email = emailField.text
-        newUser.password = passwordField.text
-        
-        // call sign up function on the object
-        newUser.signUpInBackground { (success: Bool, error: Error?) in
-            if let error = error {
-                print(error.localizedDescription)
-                self.alertDisplay(strTitle: "Alert!", strMessage: "Registration did not work, please try again later.")
-            } else {
-                print("User Registered successfully")
-                // manually segue to logged in view
-                self.performSegue(withIdentifier: "loginSegue", sender: nil)
-            }
-        }
-    }
-    
-    func loginUser() {
-        
-        let username = usernameField.text!
-        let password = passwordField.text!
-        
-        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
-            if let error = error {
-                print("User log in failed: \(error.localizedDescription)")
-                self.alertDisplay(strTitle: "Alert!", strMessage: "Invalid login, or server is down.")
-            } else {
-                print("User logged in successfully")
-                // display view controller that needs to shown after successful login
-                self.performSegue(withIdentifier: "loginSegue", sender: nil)
-            }
         }
     }
 }
