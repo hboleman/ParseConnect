da4f18483a18838923c4c59a81e9d8f487d6b63a
Hunter Boleman <hunter.boleman@gmail.com>
2019-04-12 18:20:06 -0700

    working on stuff, just a save

--- Parsechat/ChatViewController.swift
+++ Parsechat/ChatViewController.swift
@@ -17,6 +17,9 @@ class ChatViewController: UIViewController, UITableViewDataSource {
     
     // Master Message Object
     var chatMessages: [PFObject] = [];
+    var userToMatchMake: String = "";
+    var userFound: Bool = false;
+    var currentMatchMake: Int = 1;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,34 +35,30 @@ class ChatViewController: UIViewController, UITableViewDataSource {
         getChatMessages();
         print ("reload tableView")
         self.tableView.reloadData();
-    }
-    
-    // Gets Chat Messages
-    @objc func getChatMessages(){
-        let query = PFQuery(className:"Messages")
-        query.addDescendingOrder("createdAt")
-        query.limit = 20
-        query.includeKey("user")
         
-        query.findObjectsInBackground { (messages, error) in
-            if let error = error {
-                // Log details of the failure
-                print(error.localizedDescription)
-            } else if let messages = messages {
-                // The find succeeded.
-                self.chatMessages = messages
-                print("Successfully retrieved \(messages.count) posts.")
-            }
-        }
-        print ("reload tableView")
-        self.tableView.reloadData();
+        // Match Making
+        
     }
     
-    // Sends The User's Message
-    @IBAction func doSendMessage(_ sender: Any) {
-        let chatMessage = PFObject(className: "Messages");
-        chatMessage["text"] = chatMessageField.text!
+    // Start Match Making Process
+    func startMatchMaking(){
+        // Set Status Open
+        StatusOpen();
+        ListenForUsers();
+        
+        
+    }
+    
+    //Set Match Status to Open
+    func StatusOpen() {
+        let chatMessage = PFObject(className: "MatchMake");
+        //chatMessage["text"] = chatMessageField.text!
+        chatMessageField.text = "STATUS OPEN";
+        chatMessage["text"] = "STATUS:OPEN"
         chatMessage["user"] = PFUser.current();
+        chatMessage["current"] = currentMatchMake;
+        currentMatchMake = currentMatchMake + 1;
+        
         chatMessage.saveInBackground { (success, error) in
             if success {
                 print("The message was saved!")
@@ -70,39 +69,160 @@ class ChatViewController: UIViewController, UITableViewDataSource {
         }
     }
     
-    // Allows The User to Logout
-    @IBAction func doLogout(_ sender: Any) {
-        PFUser.logOutInBackground { (error) in
-            if (error != nil) {
-                print("Error, cannot logout: \(String(describing: error))")
+    // Listen for Open User
+    func ListenForUsers(){
+        var numberOfMessages: Int = 0;
+        chatMessageField.text = "LISTENING FOR USER";
+        
+        while (userFound == false){
+            let query = PFQuery(className:"MatchMake")
+            query.addDescendingOrder("createdAt")
+            query.limit = 10
+            query.includeKey("user")
+            
+            query.findObjectsInBackground { (messages, error) in
+                if let error = error {
+                    // Log details of the failure
+                    print(error.localizedDescription)
+                } else if let message = messages {
+                    // The find succeeded.
+                    self.chatMessages = message
+                    var msg: String = "";
+                    var usr: String = "";
+                    print("Successfully retrieved \(message.count) posts.")
+                    // Looking for open
+                    var countOfMessages = self.chatMessages.count;
+                    
+                    for index in 1...countOfMessages {
+                        // gets a single message
+                        let chatMessage = self.chatMessages[index];
+                        msg = (chatMessage["text"] as? String)!;
+                        usr = (chatMessage["user"] as? String)!;
+                        if (msg == "STATUS:OPEN"){
+                            print ("STATUS IS CONFIRMED OPEN");
+                            print ("OPEN: \(usr)");
+                            self.userToMatchMake = usr;
+                            self.userFound = true;
+                            
+                        }
+                    }
+                }
+                print ("reload tableView")
+                self.tableView.reloadData();
             }
         }
-        self.performSegue(withIdentifier: "LogoutSeg", sender: nil)
     }
-    
-    // TABLE VIEW FUNCTIONS
-    
-    // Sets Table Rows
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return chatMessages.count;
-    }
-    
-    // Sets Table Cell Contents
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Reusable Cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell") as! ChatCell;
-        // gets a single message
-        let chatMessage = chatMessages[indexPath.row];
-        // Set text
-        cell.messageLable.text = chatMessage["text"] as? String;
-        //Set username
-        if let user = chatMessage["user"] as? PFUser {
-            // User found! update username label with username
-            cell.usernameLabel.text = user.username;
-        } else {
-            // No user found, set default username
-            cell.usernameLabel.text = "🤖"
+        // Checks latest message of a given user and sees if that one says open or not.
+        func isUser (countOfMessages: Int){
+            // Check if is newest message
+            var messageCurrent = true;
+            
+            // Get number for latest message
+            var mostRecentMsg: Int = 0;
+            for index in 1...countOfMessages {
+                let chatMessage = self.chatMessages[index];
+                let curr = (chatMessage["current"] as? Int)!;
+                var usr_i = (chatMessage["user"] as? String)!;
+                // Find latest message
+                if (usr_i == self.userToMatchMake){
+                    mostRecentMsg = curr;
+                }
+            }
+            // Find latest message and see if open
+            for index in 1...countOfMessages {
+                let chatMessage = self.chatMessages[index];
+                let curr = (chatMessage["current"] as? Int)!;
+                var usr_i = (chatMessage["user"] as? String)!;
+                var msg_i = (chatMessage["text"] as? String)!;
+                var mostRecentMsg: Int = 0;
+                
+                if (usr_i == self.userToMatchMake && curr == mostRecentMsg){
+                    if (msg_i == "STATUS:OPEN"){
+                        print ("FOUND: OPEN AND RECENT")
+                    }
+                    else {
+                        print ("REJECT: OPEN BUT NOT RECENT")
+                    }
+                    
+                }
+            }
         }
-        return cell;
+        
+        // Handshake's with Other User
+        func InitiateHandshake(){
+            
+        }
+        
+        // Gets Chat Messages
+        @objc func getChatMessages(){
+            let query = PFQuery(className:"Messages")
+            query.addDescendingOrder("createdAt")
+            query.limit = 20
+            query.includeKey("user")
+            
+            query.findObjectsInBackground { (messages, error) in
+                if let error = error {
+                    // Log details of the failure
+                    print(error.localizedDescription)
+                } else if let messages = messages {
+                    // The find succeeded.
+                    self.chatMessages = messages
+                    print("Successfully retrieved \(messages.count) posts.")
+                }
+            }
+            print ("reload tableView")
+            self.tableView.reloadData();
+        }
+        
+        // Sends The User's Message
+        @IBAction func doSendMessage(_ sender: Any) {
+            let chatMessage = PFObject(className: "Messages");
+            chatMessage["text"] = chatMessageField.text!
+            chatMessage["user"] = PFUser.current();
+            chatMessage.saveInBackground { (success, error) in
+                if success {
+                    print("The message was saved!")
+                    self.chatMessageField.text = "";
+                } else if let error = error {
+                    print("Problem saving message: \(error.localizedDescription)")
+                }
+            }
+        }
+        
+        // Allows The User to Logout
+        @IBAction func doLogout(_ sender: Any) {
+            PFUser.logOutInBackground { (error) in
+                if (error != nil) {
+                    print("Error, cannot logout: \(String(describing: error))")
+                }
+            }
+            self.performSegue(withIdentifier: "LogoutSeg", sender: nil)
+        }
+        
+        // TABLE VIEW FUNCTIONS
+        
+        // Sets Table Rows
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return chatMessages.count;
+        }
+        
+        // Sets Table Cell Contents
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            // Reusable Cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell") as! ChatCell;
+            // gets a single message
+            let chatMessage = chatMessages[indexPath.row];
+            // Set text
+            cell.messageLable.text = chatMessage["text"] as? String;
+            //Set username
+            if let user = chatMessage["user"] as? PFUser {
+                // User found! update username label with username
+                cell.usernameLabel.text = user.username;
+            } else {
+                // No user found, set default username
+                cell.usernameLabel.text = "🤖"
+            }
+            return cell;
+        }
     }
-}
+
